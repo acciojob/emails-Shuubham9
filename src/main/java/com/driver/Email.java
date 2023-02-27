ac3c4e4ae323bd4dd5ea5e52e7fe1c03867ceb7d
shuubham <vaggysv@gmail.com>
2023-02-27 10:39:47 +0530

    email assignment

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -10,6 +10,7 @@ public class Email {
         this.password = "Accio@123";
     }
 
+
     public String getEmailId() {
         return emailId;
     }
@@ -25,5 +26,69 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword.equals(this.password)){
+            if(isValid(newPassword)) password=newPassword;
+        }
+    }
+
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+
+    public boolean isValid(String pas){
+        if(pas.length()<8){
+            return false;
+        }
+        if(!isUppercase(pas)){
+          return false;
+        }
+        if(!isLowercase(pas)){
+            return false;
+        }
+        if(!isDigit(pas)){
+            return false;
+        }
+        if(!isSpecial(pas)){
+            return false;
+        }
+        return true;
+    }
+    public boolean isUppercase(String pas){
+        for(int i=0;i<pas.length();i++){
+            if(Character.isUpperCase(pas.charAt(i))){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isLowercase(String pas){
+        for(int i=0;i<pas.length();i++){
+            if(Character.isLowerCase(pas.charAt(i))){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isDigit(String pas){
+        for(int i=0;i<pas.length();i++){
+            if(Character.isDigit(pas.charAt(i))){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean isSpecial(String pas){
+        for(int i=0;i<pas.length();i++){
+            if(Character.isDigit(pas.charAt(i))){
+                continue;
+            }
+            else if (Character.isLetter(pas.charAt(i))) {
+                continue;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -3,8 +3,7 @@ package com.driver;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.*;
 
 public class Workspace extends Gmail{
 
@@ -12,19 +11,40 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-
+       super(emailId,Integer.MAX_VALUE);
+       calendar= new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-
+        calendar.add(meeting);
     }
 
-    public int findMaxMeetings(){
+    public int findMaxMeetings() {
         // find the maximum number of meetings you can attend
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
+        List<Pair<LocalTime,Integer>> endtimes = new ArrayList<>();
+        for(int i=0;i<calendar.size();i++){
+            endtimes.add(Pair.of(calendar.get(i).getEndTime(),i));
+        }
 
+        Collections.sort(endtimes);
+
+        LocalTime temp = endtimes.get(0).getLeft();
+        int cnt=0;
+        if(!endtimes.isEmpty()){
+            cnt+=1;
+        }
+        for(int i=1;i<calendar.size();i++){
+          if(calendar.get(endtimes.get(i).getRight()).getStartTime().compareTo(temp)>0){
+              cnt++;
+          }
+          temp=endtimes.get(i).getLeft();
+        }
+
+
+        return cnt;
     }
 }
